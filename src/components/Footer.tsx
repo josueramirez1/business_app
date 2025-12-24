cc5e48d628b49dadf1225146d70ec61017f4b36b
Josue Ramirez <josue67ramirez@gmail.com>
2025-12-23 17:10:19 -0800

    styling footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,194 @@
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-      <aside>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
-        <p>
-          ACME Industries Ltd.
-          <br />
-          Providing reliable tech since 1992
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <img
+            src="../public/upscaler-logo.png"
+            className="w-auto h-10"
+            alt=""
+          />
+        </div>
+
+        <div className="grid grid-cols-1 gap-8 border-t border-gray-100 pt-8 sm:grid-cols-2 lg:grid-cols-4 lg:pt-16">
+          <div>
+            <p className="font-medium text-gray-900">Services</p>
+
+            <ul className="mt-6 space-y-4 text-sm">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  1on1 Coaching{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Company Review{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Accounts Review{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  HR Consulting{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  SEO Optimisation{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p className="font-medium text-gray-900">Company</p>
+
+            <ul className="mt-6 space-y-4 text-sm">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  About{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Meet the Team{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Accounts Review{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p className="font-medium text-gray-900">Helpful Links</p>
+
+            <ul className="mt-6 space-y-4 text-sm">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Contact{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  FAQs{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Live Chat{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p className="font-medium text-gray-900">Legal</p>
+
+            <ul className="mt-6 space-y-4 text-sm">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Accessibility{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Returns Policy{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  {" "}
+                  Refund Policy{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition hover:opacity-75"
+                >
+                  Hiring-3 Statistics
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <p className="text-xs text-gray-500">
+          © 2026. Company Name. All rights reserved.
         </p>
-      </aside>
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
+      </div>
     </footer>
   );
 };
